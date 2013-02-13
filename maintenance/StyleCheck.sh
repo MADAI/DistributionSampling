cd889f8b8b34cdeafd8265e4870bb4b53c6fa995
Cory Quammen <cquammen@cs.unc.edu>
2013-02-13 10:19:19 -0500

    Removed empty lines to cut down on style check output

--- maintenance/StyleCheck.sh
+++ maintenance/StyleCheck.sh
@@ -3,6 +3,10 @@
 # To use, run this from the top level of the DistributionSampling
 # library source directory.
 
+script_path="`dirname \"$0\"`"
+
 files=`ls -1 src/*.h src/*.cxx`
 
-for file in $files; do KWStyle -v $file; done
+for file in $files; do KWStyle -v $file -xml ${script_path}/KWStyle.xml | sed '/^$/d'; done
+
+
