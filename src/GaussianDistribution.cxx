6f961ff5d9b0ecbe8f6008a192ef8d262bd7cb39
Cory Quammen <cquammen@cs.unc.edu>
2013-03-06 01:25:38 -0500

    Added GaussianDistribution class
    The implementation of GetPercentile() is incomplete. It returns valid
    values for inputs of 0.25 and 0.75 only. Otherwise it returns 0. Need
    to replace this with general percentile calculation.
    
    Also, need to add test.

---
+++ src/GaussianDistribution.cxx
@@ -0,0 +1,125 @@
+/*=========================================================================
+ *
+ *  Copyright The University of North Carolina at Chapel Hill
+ *  All rights reserved.
+ *
+ *  Licensed under the MADAI Software License. You may obtain a copy of
+ *  this license at
+ *
+ *         https://madai-public.cs.unc.edu/software/license/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "GaussianDistribution.h"
+
+#include <cmath>
+
+
+namespace madai {
+
+GaussianDistribution
+::GaussianDistribution() :
+  m_Mean( 0.0 ),
+  m_StandardDeviation( 1.0 )
+{
+}
+
+
+GaussianDistribution
+::~GaussianDistribution()
+{
+}
+
+
+void
+GaussianDistribution
+::SetMean( double mean )
+{
+  m_Mean = mean;
+}
+
+
+double
+GaussianDistribution
+::GetMean() const
+{
+  return m_Mean;
+}
+
+
+void
+GaussianDistribution
+::SetStandardDeviation( double standardDeviation )
+{
+  m_StandardDeviation = standardDeviation;
+}
+
+
+double
+GaussianDistribution
+::GetStandardDeviation() const
+{
+  return m_StandardDeviation;
+}
+
+
+double
+GaussianDistribution
+::GetLogProbabilityDensity( double value ) const
+{
+  return log( this->GetNormalizationFactor() ) + this->GetExponent( value );
+}
+
+
+double
+GaussianDistribution
+::GetProbabilityDensity( double value ) const
+{
+  return this->GetNormalizationFactor() * exp( this->GetExponent( value ) );
+}
+
+
+double
+GaussianDistribution
+::GetPercentile( double percentile ) const
+{
+  // \todo - Implement general version. The percentiles we need most
+  // often are defined here.
+  if ( percentile == 0.25 ) {
+    return -0.67448 * m_StandardDeviation + m_Mean;
+  } else if ( percentile == 0.75 ) {
+    return 0.67448 * m_StandardDeviation + m_Mean;
+  }
+
+  return 0.0;
+}
+
+
+double
+GaussianDistribution
+::GetNormalizationFactor() const
+{
+  double variance = m_StandardDeviation * m_StandardDeviation;
+
+  return (1.0 / sqrt( 2.0 * M_PI * variance ) );
+}
+
+
+double
+GaussianDistribution
+::GetExponent( double value ) const
+{
+  double variance = m_StandardDeviation * m_StandardDeviation;
+  double exponent = -( value - m_Mean ) * ( value - m_Mean ) / ( 2.0 * variance );
+
+  return exponent;
+}
+
+
+} // end namespace madai
