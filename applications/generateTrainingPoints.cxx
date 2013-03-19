4a762dc3ba4585d255c96bda588fbe204f5b1271
Cory Quammen <cquammen@cs.unc.edu>
2013-03-19 14:22:48 -0400

    Added reading of parameter_priors.dat file

--- applications/generateTrainingPoints.cxx
+++ applications/generateTrainingPoints.cxx
@@ -16,8 +16,14 @@
  *
  *=========================================================================*/
 
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
+
+#include "GaussianDistribution.h"
+#include "Parameter.h"
+#include "UniformDistribution.h"
+
 
 static const char usage[] =
   "usage:\n"
@@ -42,19 +48,19 @@ static const char usage[] =
   "  -v,--verbose Print extra information\n"
   "\n";
 
-struct cmdLineOpts {
+struct CommandLineOptions {
   bool verbose;
   const char * parametersFile;  // First non-flag argument.
   const char * outputDirectory; // Second non-flag argument.
 };
 
 
-bool parseCommandLineOptions( int argc, char* argv[], struct cmdLineOpts & opts)
+bool ParseCommandLineOptions( int argc, char* argv[], struct CommandLineOptions & options)
 {
   // Default values for command-line options
-  opts.verbose = false;
-  opts.parametersFile = NULL;
-  opts.outputDirectory = NULL;
+  options.verbose = false;
+  options.parametersFile = NULL;
+  options.outputDirectory = NULL;
 
   // Parse options
   int argIndex;
@@ -62,7 +68,7 @@ bool parseCommandLineOptions( int argc, char* argv[], struct cmdLineOpts & opts)
     std::string argString( argv[ argIndex ] );
 
     if ( argString == "-v" || argString == "--verbose" ) {
-      opts.verbose = true;
+      options.verbose = true;
     } else if ( argString == "-h" || argString == "--help" ) {
       std::cerr << usage;
       return false;
@@ -72,16 +78,21 @@ bool parseCommandLineOptions( int argc, char* argv[], struct cmdLineOpts & opts)
   }
 
   if ( argIndex < argc ) {
-    opts.parametersFile = argv[ argIndex++ ];
+    options.parametersFile = argv[ argIndex++ ];
   } else {
     std::cerr << usage;
     return false;
   }
 
   if ( argIndex < argc ) {
-    opts.outputDirectory = argv[ argIndex++ ];
+    options.outputDirectory = argv[ argIndex++ ];
   } else {
     std::cerr << usage;
+    return false;
+  }
+
+  if ( argIndex < argc ) {
+    std::cerr << "Extra arguments after '" << options.outputDirectory << "'\n";
     return false;
   }
 
@@ -90,9 +101,110 @@ bool parseCommandLineOptions( int argc, char* argv[], struct cmdLineOpts & opts)
 }
 
 
+std::string LowerCase( char * buffer )
+{
+  std::string outBuffer( buffer );
+
+  std::transform( outBuffer.begin(), outBuffer.end(),
+                  outBuffer.begin(), ::tolower );
+
+  return outBuffer;
+}
+
+
+bool ReadParameters( const struct CommandLineOptions & options,
+                     std::vector< madai::Parameter > & parameters )
+{
+  parameters.clear();
+
+  FILE *fp = fopen( options.parametersFile, "r" );
+  if ( !fp ) {
+    return false;
+  }
+
+  if ( options.verbose ) {
+    std::cout << "Opened file '" << options.parametersFile << "'\n";
+  }
+
+  char buffer[4096];
+
+  // Parse each parameter as it is listed on a line
+  while ( 1 == fscanf( fp, "%4095s", buffer ) ) {
+    std::string parameterName( buffer );
+
+    if ( options.verbose ) {
+      std::cout << "Read parameter '" << parameterName << "' ";
+    }
+
+    if ( 1 != fscanf( fp, "%4095s", buffer ) ) {
+      std::cerr << "Could not read distribution type\n";
+      fclose( fp );
+      return false;
+    }
+
+    std::string distributionType( LowerCase( buffer ) );
+
+    if ( options.verbose ) {
+      std::cout << "which is of type '" << distributionType << "' ";
+    }
+
+    madai::Distribution *distribution = NULL;
+    madai::UniformDistribution uniform;
+    madai::GaussianDistribution gaussian;
+
+    if ( distributionType == "uniform" ) {
+      // Parse minimum and maximum values
+      double minimum, maximum;
+      if ( 2 != fscanf( fp, "%lf %lf", &minimum, &maximum ) ) {
+        std::cerr << "Could not read uniform distribution minimum and maximum\n";
+        fclose( fp );
+        return false;
+      }
+
+      if ( options.verbose ) {
+        std::cout << "with range [" << minimum << ", " << maximum << "]\n";
+      }
+
+      uniform.SetMinimum( minimum );
+      uniform.SetMaximum( maximum );
+      distribution = &uniform;
+
+    } else if ( distributionType == "gaussian" ) {
+      // Parse mean and standard deviation
+      double mean, standardDeviation;
+      if ( 2 != fscanf( fp, "%lf %lf", &mean, &standardDeviation ) ) {
+        std::cerr << "Could not read Gaussian distribution mean and standard deviation\n";
+        fclose( fp );
+        return false;
+      }
+
+      if ( options.verbose ) {
+        std::cout << "with mean " << mean << " and standard deviation "
+                  << standardDeviation << "\n";
+      }
+
+      gaussian.SetMean( mean );
+      gaussian.SetStandardDeviation( standardDeviation );
+      distribution = &gaussian;
+    } else {
+      std::cerr << "Unknown distribution type '" << distributionType << "'\n";
+      fclose( fp );
+      return false;
+    }
+
+    parameters.push_back( madai::Parameter( parameterName, *distribution ) );
+  }
+
+  fclose( fp );
+
+  // Everything went okay
+  return true;
+}
+
+
 int main( int argc, char * argv[] ) {
-  struct cmdLineOpts options;
-  if ( !parseCommandLineOptions( argc, argv, options ) ) {
+  struct CommandLineOptions options;
+  if ( !ParseCommandLineOptions( argc, argv, options ) ) {
     return EXIT_FAILURE;
   }
 
@@ -102,6 +214,16 @@ int main( int argc, char * argv[] ) {
     std::cout << "  ParametersFile: " << options.parametersFile << "\n";
     std::cout << "  OutputDirectory: " << options.outputDirectory << "\n";
   }
+
+  // Read in parameter priors
+  std::vector< madai::Parameter > parameters;
+  bool parametersRead = ReadParameters( options, parameters );
+  if ( !parametersRead ) {
+    std::cerr << "Could not read parameters from prior file '"
+              << options.parametersFile << "'" << std::endl;
+    return EXIT_FAILURE;
+  }
+
 
   return EXIT_SUCCESS;
 }
