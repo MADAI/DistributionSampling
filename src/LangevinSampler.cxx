484795c0a85fce63f0cab668c038976be8ae32b5
Jeff Wyka <wykajeff@msu.edu>
2013-03-25 07:44:22 -0400

    Created a Langevin Sampler: - Follows the equations of motion of a particle in a potential where the potential is the likelihood - Particle gets a random kick. User can set the mean time between kicks and the kick strength. - User can set a mass scale, the kick strength, and the size of the time step. - Keeps track of the current parameters and current velocities - Keeps track of the time before the next kick. (Don't currently have the kicks happening at the exact times)   -- If the kick happens within the time step, the kick is assumbed to be at the beginning of the timestep. - Gets the gradient of the logLikelihood from the model. - Currently calculates the gradient of the likelihood by multiplying the gradient of the logLikelihood by the likelihood. - Acceleration of the particle is equal to the gradient of the likelihood, the damping force proportional to the velocity.   -- Kick strength is also added to the accel which is a bug I need to fix. (The kick is an instantaneous impulse and should be added directly to the velocity) - Steps are taken in parameterspace by expanding the eqautions r(t+dt) and v(r+dt) in taylor series and using up to the second order in r and the first order in v.   -- Keep only up to the accelleration term. - NextSample returns a Sample containing the parameters, the scalar outputs, and the loglikelihood.

---
+++ src/LangevinSampler.cxx
@@ -0,0 +1,165 @@
+/*=========================================================================
+ *
+ *  Copyright The University of North Carolina at Chapel Hill
+ *  All rights reserved.
+ *
+ *  Licensed under the MADAI Software License. You may obtain a copy of
+ *  this license at
+ *
+ *         https://madai-public.cs.unc.edu/software/license/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "LangevinSampler.h"
+
+#include <cassert>
+
+namespace madai {
+
+
+LangevinSampler
+::LangevinSampler() :
+  Sampler(),
+  m_TimeStep( 1.0e-2 ),
+  m_MassScale( 10 ),
+  m_MeanTime( 0.1 ),
+  m_KickStrength( 1 ),
+  m_DragCoefficient( 1.0e-2 )
+{
+}
+
+
+LangevinSampler
+::~LangevinSampler()
+{
+}
+
+
+void
+LangevinSampler
+::Initialize( const Model * model )
+{
+  assert( model != NULL );
+  m_Model = model;
+  
+  Sampler::Initialize( model );
+  
+  // Random initial starting point
+  const std::vector< Parameter > & params = m_Model->GetParameters();
+  for ( unsigned int i = 0; i < m_Model->GetNumberOfParameters(); i++ ) {
+    const Distribution * priorDist = params[i].GetPriorDistribution();
+    m_CurrentParameters[i] = priorDist->GetSample(m_Random);
+    m_CurrentVelocities.push_back(0.0);
+  }
+  
+  m_TimeBeforeNextKick = m_Random.Gaussian( m_MeanTime, m_MeanTime / 10.0 );
+}
+
+
+Sample
+LangevinSampler
+::NextSample()
+{
+  Model * m = const_cast< Model * >(m_Model);
+   
+  unsigned int numberOfActiveParameters = this->GetNumberOfActiveParameters();
+   
+  assert( std::count( m_ActiveParameterIndices.begin(),
+                      m_ActiveParameterIndices.end(), true )
+          == numberOfActiveParameters);
+   
+  // Get the gradient of the Log Likelihood at the current point
+  std::vector< double > ModelMeans;
+  std::vector< double > Gradient;
+  m->GetScalarAndGradientOutputs(
+    m_CurrentParameters, m_ActiveParameterIndices,
+    ModelMeans, Gradient );
+    
+  // Multiply by likelihood to get gradient of the likelihood
+  double LogLikelihood;
+  m->GetScalarOutputsAndLogLikelihood(
+    m_CurrentParameters, ModelMeans, LogLikelihood );
+  for ( unsigned int i = 0; i < Gradient.size(); i++ ) {
+    Gradient[i] *= exp( LogLikelihood );
+  }
+  
+  std::vector< double > Accel( Gradient.size() );
+  for ( unsigned int i = 0; i < Accel.size(); i++ ) {
+    Accel[i] = Gradient[i];
+    Accel[i] -= m_DragCoefficient * m_CurrentVelocities[i];
+  }
+  if ( m_TimeBeforeNextKick < m_TimeStep ) { // Check if kick happens
+    // Get Accel.size() number of random numbers
+    double r[Accel.size()];
+    double temp = 0;
+    for ( unsigned int i = 0; i < Accel.size(); i++ ) {
+      r[i] = m_Random.Uniform( -1, 1 );
+      temp += r[i] * r[i];
+    }
+    temp = sqrt( temp );
+    temp /= m_KickStrength;
+    for ( unsigned int i = 0; i < Accel.size(); i++ ) {
+      Accel[i] += r[i] / temp;
+    }
+    m_TimeBeforeNextKick += m_Random.Gaussian( m_MeanTime, m_MeanTime / 10.0 );
+  }
+  for ( unsigned int i = 0; i < Accel.size(); i++ ) {
+    Accel[i] /= m_MassScale;
+  }
+  
+  for ( unsigned int i = 0; i < m_CurrentParameters.size(); i++ ) {
+    m_CurrentParameters[i] += m_CurrentVelocities[i] * m_TimeStep;
+    m_CurrentParameters[i] += 0.5 * Accel[i] * m_TimeStep * m_TimeStep;
+    m_CurrentVelocities[i] += Accel[i] * m_TimeStep;
+  }
+  
+  m_TimeBeforeNextKick -= m_TimeStep;
+  
+  return Sample( m_CurrentParameters,
+                 ModelMeans,
+                 LogLikelihood );
+
+}
+  
+  
+void LangevinSampler
+::SetTimeStep( double TimeStep )
+{
+  m_TimeStep = TimeStep;
+}
+
+
+void LangevinSampler
+::SetKickStrength( double KickStrength )
+{
+  m_KickStrength = KickStrength;
+}
+
+
+void LangevinSampler
+::SetMeanTimeBetweenKicks( double MeanTime )
+{
+  m_MeanTime = MeanTime;
+}
+
+
+void LangevinSampler
+::SetDragCoefficient( double DragCoefficient )
+{
+  m_DragCoefficient = DragCoefficient;
+}
+  
+
+void LangevinSampler
+::SetMassScale( double MassScale )
+{
+  m_MassScale = MassScale;
+}
+  
+}
