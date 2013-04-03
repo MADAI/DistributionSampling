a00dbd8e4a33f0f52fe777eab447179c651556bc
Jeff Wyka <wykajeff@msu.edu>
2013-04-03 11:38:16 -0400

    GaussianProcessEmulator: - Don't want to have to convert the data set up in the directory structure to a single file in order to use the emulator. - Defined the following functions: -- bool parseParameters( std::vector<Parameter> & parameters, int & NumberParameters, std::string Analysis_Dir ) --- This function reads the file parameter_priors.dat from the analysis directory -- bool parseModelDataDirectoryStructure( GaussianProcessEmulator & gpme, std::string Model_Outs_Dir, std::string Analysis_Dir ) --- This function reads through the directory structure, filling the emulator with the model data ( Parameters, OutputNames, ParameterValues, OutputValues, their sizes, and the NumberOfTrainingPoints) -- bool parsePCADecomposition( GaussianProcessEmulator & gpme, std::istream & input ) --- This function reads in a PCADecomposition from input ( Output Means and Uncertainty Scales, PCA Eigenvectors and Eigenvalues, and the Submodels ) -- bool parseGaussianProcessEmulator( GaussianProcessEmulator & gpme, std::string TopDirectory, bool LoadPCA ) --- This function parses the entire emulator using the directory structure of the model outputs. It will load a PCA decomposition if LoadPCA is true.

--- src/GaussianProcessEmulator.cxx
+++ src/GaussianProcessEmulator.cxx
@@ -26,6 +26,8 @@
 
 #include <cmath>        // std::exp std::amp
 #include <limits>       // std::numeric_limits
+#include <sys/stat.h>   // sys::stat
+#include <fstream>      // std::ofstream std::ifstream
 #include "GaussianProcessEmulator.h"
 #include "UniformDistribution.h"
 #include "GaussianDistribution.h"
@@ -230,6 +232,48 @@ bool parseCovarianceFunction(
 }
 
 /**
+   Read the parameter_priors.dat file in statistical_analysis. */
+bool parseParameters(
+    std::vector< madai::Parameter > & parameters,
+    int & numberParameters,
+    std::string AnalysisDir ) {
+  // First check to see if file exists
+  std::string PriorFileName = AnalysisDir+"/parameter_priors.dat";
+  std::ifstream input( PriorFileName.c_str() );
+  if ( !input.good() ) return false;
+  parameters.clear(); // Empty the output vector
+  while ( !input.eof() ) {
+    while ( input.peek() == '#' ) { // Disregard commented lines
+      std::string s;
+      std::getline( input, s );
+    }
+    std::string name;
+    std::string type;
+    double dist_vals[2];
+    input >> name >> type >> dist_vals[0] >> dist_vals[1];
+    if ( type == "uniform" ) {
+      madai::UniformDistribution priorDist;
+      priorDist.SetMinimum(dist_vals[0]);
+      priorDist.SetMaximum(dist_vals[1]);
+      parameters.push_back(madai::Parameter( name, priorDist ) );
+    } else if ( type == "gaussian" ) {
+      madai::GaussianDistribution priorDist;
+      priorDist.SetMean(dist_vals[0]);
+      priorDist.SetStandardDeviation(dist_vals[1]);
+      parameters.push_back(madai::Parameter( name, priorDist ) );
+    } else {
+      std::cerr << "Expected uniform or gaussian, but got " <<
+        type << "\n";
+      return false;
+    }
+  }
+  numberParameters = parameters.size();
+  assert( numberParameters > 0 );
+  
+  return true;
+}
+
+/**
    Read an integer from the input, followed by that number of
    parameters.  Populate the numberParameters vector. */
 bool parseParameters(
@@ -298,6 +342,14 @@ std::ostream & serializeParameters(
 bool parseOutputs(
     std::vector< std::string > & outputNames,
     int & numberOutputs,
+    std::string AnalysisDir ) {
+  
+  return false;
+}
+
+bool parseOutputs(
+    std::vector< std::string > & outputNames,
+    int & numberOutputs,
     std::istream & input) {
   if (! input.good()) return false;
   input >> numberOutputs;
@@ -323,6 +375,27 @@ bool parseInteger( int & x, std::istream & input ) {
   if (! input.good()) return false;
   input >> x;
   return true;
+}
+
+bool parseNumberOfModelRuns( int & x, std::string ModelOutDir ) {
+  
+  return false;
+}
+
+template < typename TDerived >
+inline bool parseParameterValues(
+    const Eigen::MatrixBase< TDerived > & m_,
+    std::string ModelOutDir ) {
+  
+  return false;
+}
+
+template < typename TDerived >
+inline bool parseOutputValues(
+    const Eigen::MatrixBase< TDerived > & m_,
+    std::string ModelOutDir ) {
+  
+  return false;
 }
 
 bool parseSubmodels(
@@ -412,6 +485,120 @@ std::ostream & serializeGaussianProcessEmulator(
   }
   o << "END_OF_FILE\n";
   return o;
+}
+  
+bool parseModelDataDirectoryStructure(
+    GaussianProcessEmulator & gpme,
+    std::string Model_Outs_Dir,
+    std::string Stat_Anal_Dir) {
+  if ( !parseParameters( 
+          gpme.m_Parameters, gpme.m_NumberParameters, Stat_Anal_Dir ) ) {
+    std::cerr << "parse Parameters error\n";
+    return false;
+  }
+  if ( !parseOutputs( 
+          gpme.m_OutputNames, gpme.m_NumberOutputs, Stat_Anal_Dir ) ) {
+    std::cerr << "parse Outputs error\n";
+    return false;
+  }
+  if ( !parseNumberOfModelRuns( 
+          gpme.m_NumberTrainingPoints, Model_Outs_Dir ) ) {
+    std::cerr << "parse Integer error\n";
+    return false;
+  }
+  if ( !parseParameterValues( 
+          gpme.m_ParameterValues, Model_Outs_Dir ) ) {
+    std::cerr << "parse Parameter Values error\n";
+    return false;
+  }
+  if ( !parseOutputValues( 
+          gpme.m_OutputValues, Model_Outs_Dir ) ) {
+    std::cerr << "parse Output Values error\n";
+    return false;
+  }
+  return true;
+}
+
+bool parsePCADecomposition(
+    GaussianProcessEmulator & gpme,
+    std::istream & input ) {
+  parseComments(gpme.m_Comments,input);
+  std::string word;
+  if (! CheckWord(input, "VERSION")) return false;
+  if (! CheckInteger(input, 1, "versn")) return false;
+  while (input.good()) {
+    if (! input.good()) return false;
+    input >> word;
+    if (word == "OUTPUT_MEANS") {
+      if (! ReadVector(gpme.m_OutputMeans, input)) {
+        std::cerr << "parse error\n"; // \todo error message
+        return false;
+      }
+    } else if (word == "OUTPUT_UNCERTAINTY_SCALES") {
+      if (! ReadVector(gpme.m_OutputUncertaintyScales, input)) {
+        std::cerr << "parse error\n"; // \todo error message
+        return false;
+      }
+    } else if (word == "OUTPUT_PCA_EIGENVALUES") {
+      if (! ReadVector(gpme.m_PCAEigenvalues, input)) {
+        std::cerr << "parse error\n"; // \todo error message
+        return false;
+      }
+    } else if (word == "OUTPUT_PCA_EIGENVECTORS") {
+      if (! ReadMatrix(gpme.m_PCAEigenvectors, input)) {
+        std::cerr << "parse error\n"; // \todo error message
+        return false;
+      }
+    } else if (word == "SUBMODELS") {
+      if (! parseInteger(gpme.m_NumberPCAOutputs, input)) {
+        std::cerr << "parse error\n"; // \todo error message
+        return false;
+      }
+      gpme.m_PCADecomposedModels.resize(gpme.m_NumberPCAOutputs);
+      for (int i = 0; i < gpme.m_NumberPCAOutputs; ++i) {
+        if (! parseSubmodels(gpme.m_PCADecomposedModels[i],i,input)) {
+          std::cerr << "parse error\n"; // \todo error message
+          return false;
+        }
+        gpme.m_PCADecomposedModels[i].m_Parent = &gpme;
+      }
+    } else if (word == "END_OF_FILE") {
+      return true;
+    } else {
+      std::cerr << "Unexected keyword: \"" << word << "\"\n";
+      return false;
+    }
+  }
+  return false;
+}
+
+
+bool parseGaussianProcessEmulator(
+    GaussianProcessEmulator & gpme,
+    std::string TopDirectory,
+    bool LoadPCA ) {
+  std::string MOD = TopDirectory+"/model_outputs/";
+  std::string SAD = TopDirectory+"/statistical_analysis/";
+  if ( !parseModelDataDirectoryStructure( gpme, MOD, SAD ) ) {
+    std::cerr << "parse directory structure error\n";
+    return false;
+  }
+  std::string PCADecompFileName = SAD+"PCADecomposition.dat";
+  if ( LoadPCA ) {
+    std::ifstream input( SAD.c_str() );
+    if ( input ) {
+      // File exists, read in from it
+      std::ifstream input( SAD.c_str() );
+      if ( !parsePCADecomposition( gpme, input ) ) {
+        std::cerr << "parse saved PCA Decomposition error\n";
+        return false;
+      }
+    } else {
+      std::cerr << "No saved PCA Decomposition.\n";
+      return false;
+    }
+  }
+  return true;
 }
 
 bool parseGaussianProcessEmulator(
