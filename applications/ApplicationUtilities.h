226b5a6f3a9f168b6eba82ce7ac2fbd85d203697
Cory Quammen <cquammen@cs.unc.edu>
2013-04-21 22:46:03 -0400

    Added application utilities library
    Library with useful functions for applications in the workflow.

---
+++ applications/ApplicationUtilities.h
@@ -0,0 +1,31 @@
+/*=========================================================================
+ *
+ *  Copyright 2011-2013 The University of North Carolina at Chapel Hill
+ *  All rights reserved.
+ *
+ *  Licensed under the MADAI Software License. You may obtain a copy of
+ *  this license at
+ *
+ *         https://madai-public.cs.unc.edu/software/license/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef madai_ApplicationUtilities_h_included
+#define madai_ApplicationUtilities_h_included
+
+#include <string>
+
+namespace madai {
+
+void EnsurePathSeparatorAtEnd( std::string & path );
+
+}
+
+
+#endif // madai_ApplicationUtilities_h_included
