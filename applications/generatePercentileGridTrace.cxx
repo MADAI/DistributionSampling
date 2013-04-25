f5dfaf10adb0354577f4f59cb05655c6a027849f
Cory Quammen <cquammen@cs.unc.edu>
2013-04-25 05:07:24 -0400

    Uses LoadObservations() in ApplicationUtilities now

--- applications/generatePercentileGridTrace.cxx
+++ applications/generatePercentileGridTrace.cxx
@@ -36,59 +36,6 @@ using madai::Paths;
 
 static const int DEFAULT_PERCENTILE_GRID_NUMBER_OF_SAMPLES = 100;
 
-template<class S, class T>
-int findIndex(const S & v, const T & s)
-{
-  typename S::const_iterator it = std::find(v.begin(), v.end(), s);
-  if (it == v.end())
-    return -1;
-  return std::distance(v.begin(), it);
-}
-
-/**
-   Load a file with experimental observations in it.  The model will
-   be compared against this. */
-Model::ErrorType
-LoadObservations(Model * model, std::istream & i)
-{
-  // std::ifstream i("DIRECTORY/experimental_results/results.dat");
-  const std::vector< std::string > & scalarOutputNames = model->GetScalarOutputNames();
-  unsigned int numberOfScalarOutputs = model->GetNumberOfScalarOutputs();
-  assert(scalarOutputNames.size() == numberOfScalarOutputs);
-  assert (numberOfScalarOutputs > 0);
-  std::vector< double > observedScalarValues(numberOfScalarOutputs, 0.0);
-  std::vector< double > observedScalarCovariance(
-      numberOfScalarOutputs * numberOfScalarOutputs, 0.0);
-  for (unsigned int j = 0; j < numberOfScalarOutputs; ++j)
-    observedScalarCovariance[j * (1 + numberOfScalarOutputs)] = 1.0;
-  while (true) { // will loop forever if input stream lasts forever.
-    std::string name;
-    double value, uncertainty;
-    if(! (i >> name >> value >> uncertainty))
-      break;
-    int index = findIndex(scalarOutputNames, name);
-    if (index != -1) {
-      observedScalarValues[index] = value;
-      // observedScalarCovariance is a square matrix;
-      observedScalarCovariance[index * (1 + numberOfScalarOutputs)] = std::pow(uncertainty, 2);
-      // uncertainty^2 is variance.
-    }
-  }
-  // assume extra values are all zero.
-  Model::ErrorType e;
-  e = model->SetObservedScalarValues(observedScalarValues);
-  if (e != madai::Model::NO_ERROR) {
-    std::cerr << "Error in Model::SetObservedScalarValues\n";
-    return e;
-  }
-  e = model->SetObservedScalarCovariance(observedScalarCovariance);
-  if (e != madai::Model::NO_ERROR) {
-    std::cerr << "Error in Model::SetObservedScalarCovariance\n";
-    return e;
-  }
-  return madai::Model::NO_ERROR;
-}
-
 
 int main(int argc, char ** argv) {
 
