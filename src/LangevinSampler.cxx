b3c867bf3986823d7d7e1b967eee954ca11e86e9
Hal Canary <hal@cs.unc.edu>
2013-04-29 13:34:58 -0400

    Fix pedantic warnings in LangevinSampler

--- src/LangevinSampler.cxx
+++ src/LangevinSampler.cxx
@@ -49,9 +49,9 @@ LangevinSampler
 {
   assert( model != NULL );
   m_Model = model;
-  
+
   Sampler::Initialize( model );
-  
+
   // Random initial starting point
   const std::vector< Parameter > & params = m_Model->GetParameters();
   for ( unsigned int i = 0; i < m_Model->GetNumberOfParameters(); i++ ) {
@@ -59,7 +59,7 @@ LangevinSampler
     m_CurrentParameters[i] = priorDist->GetSample(m_Random);
     m_CurrentVelocities.push_back(0.0);
   }
-  
+
   m_TimeBeforeNextKick = m_Random.Gaussian( m_MeanTime, m_MeanTime / 10.0 );
 }
 
@@ -70,26 +70,27 @@ LangevinSampler
 {
   Model * m = const_cast< Model * >(m_Model);
   double TimeLeft = m_TimeStep;
-   
+
   unsigned int numberOfActiveParameters = this->GetNumberOfActiveParameters();
-   
-  assert( std::count( m_ActiveParameterIndices.begin(),
-                      m_ActiveParameterIndices.end(), true )
+
+  assert( static_cast<unsigned int> (
+              std::count( m_ActiveParameterIndices.begin(),
+                          m_ActiveParameterIndices.end(), true ))
           == numberOfActiveParameters);
-   
+
   // Get the gradient of the Log Likelihood at the current point
   std::vector< double > Gradient;
   m->GetScalarAndGradientOutputs(
     m_CurrentParameters, m_ActiveParameterIndices,
     m_CurrentOutputs, Gradient );
-    
+
   // Get loglikelihood at the current parameters
   double LogLikelihood;
   m->GetScalarOutputsAndLogLikelihood(
     m_CurrentParameters, m_CurrentOutputs, LogLikelihood );
   // Check for NaN
   assert( LogLikelihood == LogLikelihood );
-  
+
   std::vector< double > Accel = Gradient;
   for ( unsigned int i = 0; i < Accel.size(); i++ ) {
     Accel[i] -= m_DragCoefficient * m_CurrentVelocities[i];
@@ -103,7 +104,7 @@ LangevinSampler
       m_CurrentVelocities[i] += Accel[i] * m_TimeBeforeNextKick;
     }
     // Get m_CurrentVelocities.size() number of random numbers
-    double r[m_CurrentVelocities.size()];
+    std::vector< double > r(m_CurrentVelocities.size());
     double temp = 0;
     for ( unsigned int i = 0; i < m_CurrentVelocities.size(); i++ ) {
       r[i] = m_Random.Uniform( -1, 1 );
@@ -118,22 +119,22 @@ LangevinSampler
     TimeLeft -= m_TimeBeforeNextKick;
     m_TimeBeforeNextKick += m_Random.Gaussian( m_MeanTime, m_MeanTime / 10.0 );
   }
-  
+
   for ( unsigned int i = 0; i < m_CurrentParameters.size(); i++ ) {
     m_CurrentParameters[i] += m_CurrentVelocities[i] * TimeLeft;
     m_CurrentParameters[i] += 0.5 * Accel[i] * TimeLeft * TimeLeft;
     m_CurrentVelocities[i] += Accel[i] * TimeLeft;
   }
-  
+
   m_TimeBeforeNextKick -= m_TimeStep;
-  
+
   return Sample( m_CurrentParameters,
                  m_CurrentOutputs,
                  LogLikelihood );
 
 }
-  
-  
+
+
 void LangevinSampler
 ::SetTimeStep( double TimeStep )
 {
@@ -160,7 +161,7 @@ void LangevinSampler
 {
   m_DragCoefficient = DragCoefficient;
 }
-  
+
 
 void LangevinSampler
 ::SetMassScale( double MassScale )
@@ -173,14 +174,14 @@ LangevinSampler
 ::SetVelocity( const std::string & parameterName, double Velocity )
 {
   unsigned int parameterIndex = this->GetParameterIndex( parameterName );
-  
+
   if ( parameterIndex == static_cast< unsigned int >( -1 ) ) {
     return INVALID_PARAMETER_INDEX_ERROR;
   }
-  
+
   m_CurrentVelocities[parameterIndex] = Velocity;
-  
+
   return NO_ERROR;
 }
-  
+
 } // end namespace madai
