9311bae4b3a35097082913d282dae5ad9da0ae7c
Hal Canary <hal@cs.unc.edu>
2013-04-30 22:18:03 -0400

    Issue356 - update code for KWStyle - LangevinSampler.h

--- src/LangevinSampler.h
+++ src/LangevinSampler.h
@@ -1,6 +1,6 @@
 /*=========================================================================
  *
- *  Copyright The University of North Carolina at Chapel Hill
+ *  Copyright 2011-2013 The University of North Carolina at Chapel Hill
  *  All rights reserved.
  *
  *  Licensed under the MADAI Software License. You may obtain a copy of
@@ -16,14 +16,14 @@
  *
  *=========================================================================*/
 
-#ifndef __LangevinSampler_h__
-#define __LangevinSampler_h__
+#ifndef madai_LangevinSampler_h_included
+#define madai_LangevinSampler_h_included
 
 #include "Sampler.h"
 
 
 namespace madai {
-  
+
 /**
  * \class LangevinSampler
  *
@@ -38,48 +38,48 @@ public:
   LangevinSampler();
   // Destructor
   virtual ~LangevinSampler();
-  
+
   virtual Sample NextSample();
-  
+
   virtual void SetTimeStep( double TimeStep );
   virtual double GetTimeStep() { return this->m_TimeStep; }
-  
+
   virtual void SetKickStrength( double Impulse );
   virtual double GetKickStrength() { return this->m_KickStrength; }
-  
+
   virtual void SetMeanTimeBetweenKicks( double MeanTime );
   virtual double GetMeanTimeBetweenKicks() { return this->m_MeanTime; }
-  
+
   virtual void SetDragCoefficient( double DragCoefficient );
   virtual double GetDragCoefficient() { return this->m_DragCoefficient; }
-  
+
   virtual void SetMassScale( double MassScale );
   virtual double GetMassScale() { return this->m_MassScale; }
-  
+
   virtual ErrorType SetVelocity( const std::string & parameterName, double Velocity );
   virtual std::vector< double > GetVelocities() { return m_CurrentVelocities; }
-  
+
 protected:
-  
+
   double m_TimeStep;
   double m_MeanTime;
   double m_KickStrength;
   double m_TimeBeforeNextKick;
   double m_DragCoefficient;
   double m_MassScale;
-  
+
   // Keep track of the velocities
   std::vector< double > m_CurrentVelocities;
-  
+
   virtual void Initialize( const Model * model );
 
 private:
 
   // Random number object
   madai::Random m_Random;
-  
+
 }; // end class LangevinSampler
 
 } // end namespace madai
 
-#endif // __LangevinSampler_h__
+#endif // madai_LangevinSampler_h_included
