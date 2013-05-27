bd42424c6b90ca0487cc8f8f7ad3c78c3273c079
Hal Canary <hal@cs.unc.edu>
2013-05-27 13:15:28 -0400

    GetOptions Default in madai_generate_mcmc_trace_external

--- applications/madai_generate_mcmc_trace_external.cxx
+++ applications/madai_generate_mcmc_trace_external.cxx
@@ -89,28 +89,23 @@ int main(int argc, char ** argv) {
   std::string experimentalResultsFile =
     madai::GetExperimentalResultsFile( statisticsDirectory, settings );
 
-  int numberOfSamples = madai::Defaults::MCMC_NUMBER_OF_SAMPLES;
-  if ( settings.HasOption( "MCMC_NUMBER_OF_SAMPLES" ) ) {
-    numberOfSamples =
-      atoi( settings.GetOption( "MCMC_NUMBER_OF_SAMPLES" ).c_str() );
-  }
-  int numberOfBurnInSamples = madai::Defaults::MCMC_NUMBER_OF_BURN_IN_SAMPLES;
-  if ( settings.HasOption( "MCMC_NUMBER_OF_BURN_IN_SAMPLES" ) ) {
-    numberOfBurnInSamples =
-      atoi( settings.GetOption( "MCMC_NUMBER_OF_BURN_IN_SAMPLES" ).c_str() );
-  }
-  bool useModelError = madai::Defaults::MCMC_USE_MODEL_ERROR;
-  if ( settings.HasOption( "MCMC_USE_MODEL_ERROR" ) ) {
-    useModelError = ( settings.GetOption( "MCMC_USE_MODEL_ERROR" ) == "true" );
-  }
-  double stepSize = madai::Defaults::MCMC_STEP_SIZE;
-  if ( settings.HasOption( "MCMC_STEP_SIZE" ) ) {
-    stepSize = atof( settings.GetOption( "MCMC_STEP_SIZE" ).c_str() );
-  }
-  std::string executable;
-  if ( settings.HasOption( "EXTERNAL_MODEL_EXECUTABLE" ) ) {
-    executable = settings.GetOption( "EXTERNAL_MODEL_EXECUTABLE" );
-  }
+  int numberOfSamples = settings.GetOptionAsInt(
+      "MCMC_NUMBER_OF_SAMPLES",
+      madai::Defaults::MCMC_NUMBER_OF_SAMPLES);
+
+  int numberOfBurnInSamples = settings.GetOptionAsInt(
+      "MCMC_NUMBER_OF_BURN_IN_SAMPLES",
+      madai::Defaults::MCMC_NUMBER_OF_BURN_IN_SAMPLES);
+
+  bool useModelError = settings.GetOptionAsBool(
+      "MCMC_USE_MODEL_ERROR",
+      madai::Defaults::MCMC_USE_MODEL_ERROR);
+
+  double stepSize = settings.GetOptionAsDouble(
+      "MCMC_STEP_SIZE",
+      madai::Defaults::MCMC_STEP_SIZE);
+
+  std::string executable = settings.GetOption("EXTERNAL_MODEL_EXECUTABLE", "");
 
   // Split arguments into vector of strings
   std::vector< std::string > arguments;
