149833e76b8c6895de52bafc2037d25241fb64c4
Hal Canary <hal@cs.unc.edu>
2013-06-18 11:52:27 -0400

    Use Boost_INCLUDE_DIR and EIGEN3_INCLUDE_DIR environment variables, if they exist.  Otherwise, do the default.

--- maintenance/DistributionSamplingTest.sh
+++ maintenance/DistributionSamplingTest.sh
@@ -9,25 +9,25 @@ SRC_DIR=$(cd "$(dirname "$0")/.."; pwd;)
 die() { echo "$@" >&2; exit 1; }
 try() { "$@" || die "\"$@\" failed."; }
 require() { for arg; do
-    command -v "$1" > /dev/null || die "${1} not found"; done; }
+	command -v "$1" > /dev/null || die "${1} not found"; done; }
 getcommit() (
-    cd "$1"; git show-ref -d $(git symbolic-ref HEAD) --abbrev --hash )
+	cd "$1"; git show-ref -d $(git symbolic-ref HEAD) --abbrev --hash )
 abspath() { echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"; }
 printfilelink() { echo "\"file://$(abspath "$1")\"" ; }
 getopencmd() {
-    case "$(uname -s)" in
-        *WIN*|*W32*) command -v "start";;
-        Darwin) command -v "open";;
-        *)  command -v "xdg-open" || \
-            command -v "gvfs-open" || \
-            command -v "kde-open" || \
-            command -v "exo-open" ||
+	case "$(uname -s)" in
+		*WIN*|*W32*) command -v "start";;
+		Darwin) command -v "open";;
+		*)  command -v "xdg-open" || \
+			command -v "gvfs-open" || \
+			command -v "kde-open" || \
+			command -v "exo-open" ||
 			echo printfilelink; esac; }
 get_nproc() {
 	# print out number of parallel processes
-    command -v nproc > /dev/null && { nproc; return; }
-    # Insert other logic here.
-    echo 1; ## default value
+	command -v nproc > /dev/null && { nproc; return; }
+	# Insert other logic here.
+	echo 1; ## default value
 }
 
 ## Build Function.  Returns 0 if build is successful.
@@ -38,15 +38,25 @@ build() (
 	BUILD_TYPE="$1" # Debug or Release
 	INSTALL_PREFIX="$2"
 	DEFAULT_PREFIX="/tmp/$(id -un)/local"
-    try cmake --quiet "$SRC_DIR" \
-        -DCMAKE_INSTALL_PREFIX:PATH="${INSTALL_PREFIX:-$DEFAULT_PREFIX}" \
-        -DCMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}" \
-        -DBUILD_TESTING:BOOL=1 \
-        -DUSE_OPENMP:BOOL=0 \
-        -DUSE_GPROF:BOOL=0 \
-        -DABORT_ON_COMPILER_WARNINGS:BOOL=1
-    try nice make -j"${NPROC:-$(get_nproc)}" all
-    try nice make test
+	#If Boost_INCLUDE_DIR is not /usr/include, set this
+	if [ "$Boost_INCLUDE_DIR" ]; then
+		BOOST_OPT="-DBoost_INCLUDE_DIR:PATH=${Boost_INCLUDE_DIR}"
+	fi
+	#If EIGEN3_INCLUDE_DIR is not /usr/include/eigen3, set this
+	if [ "$EIGEN3_INCLUDE_DIR" ]; then
+		EIGEN3_OPT="-DEIGEN3_INCLUDE_DIR:PATH=${EIGEN3_INCLUDE_DIR}"
+	fi
+	try cmake --quiet \
+		-DCMAKE_INSTALL_PREFIX:PATH="${INSTALL_PREFIX:-$DEFAULT_PREFIX}" \
+		-DCMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}" \
+		-DBUILD_TESTING:BOOL=1 \
+		-DUSE_OPENMP:BOOL=0 \
+		-DUSE_GPROF:BOOL=0 \
+		-DABORT_ON_COMPILER_WARNINGS:BOOL=1 \
+		"$BOOST_OPT" "$EIGEN3_OPT" \
+		"$SRC_DIR"
+	try nice make -j"${NPROC:-$(get_nproc)}" all
+	try nice make test
 	if [ "$INSTALL_PREFIX" ] ; then
 		try mkdir -p "$INSTALL_PREFIX"
 		try rm -rf "${INSTALL_PREFIX}/lib/madai"
@@ -101,27 +111,27 @@ parabolic_test() (
 	try cp "${PARABEXDIR}/observable_names.dat" .
 	try cp "${PARABEXDIR}/parameter_priors.dat" .
 	cat > ./experimental_results.dat <<-EOF
-	MEAN_X         1.14           0.1
+	MEAN_X		 1.14		   0.1
 	MEAN_X_SQUARED 2.77634418605  0.1
-	MEAN_ENERGY    3.4925         0.1
+	MEAN_ENERGY	3.4925		 0.1
 	# HELLO WORLD
 	EOF
 	try madai_print_default_settings > ./settings.dat
 	try madai_set_variable . VERBOSE 1
 
 	if [ "$MODE" = "interact" ] ; then
-	    try madai_set_variable . EXTERNAL_MODEL_EXECUTABLE \
+		try madai_set_variable . EXTERNAL_MODEL_EXECUTABLE \
 			"${PARABEXDIR}/parabolic_interactive.py"
 	else
-	    try madai_set_variable . GENERATE_TRAINING_POINTS_NUMBER_OF_POINTS 100
-	    try madai_set_variable . GENERATE_TRAINING_POINTS_USE_MAXIMIN 1
-	    try madai_generate_training_points . > /dev/null
-	    try python "${PARABEXDIR}/parabolic_evaluate.py" \
+		try madai_set_variable . GENERATE_TRAINING_POINTS_NUMBER_OF_POINTS 100
+		try madai_set_variable . GENERATE_TRAINING_POINTS_USE_MAXIMIN 1
+		try madai_generate_training_points . > /dev/null
+		try python "${PARABEXDIR}/parabolic_evaluate.py" \
 			./model_output/run* > /dev/null
-	    try madai_pca_decompose .
-	    #
+		try madai_pca_decompose .
+
 		try madai_set_variable . EMULATOR_SCALE 0.025
-	    try madai_train_emulator .
+		try madai_train_emulator .
 	fi
 
 	try madai_set_variable . MCMC_USE_MODEL_ERROR 0
